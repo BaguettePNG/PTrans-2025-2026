0cf7ff64c5fbc04e95eb4f0c79c4100200b4b43a
Nolan <saturnsaturnsaturn17@gmail.com>
2025-11-28 21:53:32 +0100

    Archi_2

--- ESP32_TEST/src/main.cpp
+++ ESP32_TEST/src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
 
+unsigned long start;
+unsigned long stop;
+
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);         
 }
 
 void loop()
 {
-    
+  start = micros();
+
+  // Votre Codes
+
+  stop = micros();
+  unsigned long executionTime = stop - start;
+
+  Serial.print(" | Temps d'exécution: ");
+  Serial.print(executionTime);
+  Serial.println(" μs");
+  Serial.print(executionTime / 1000.0, 3);
+  Serial.println(" ms");
 }
