8580dd680dd4dc292022eef6b67778088b4a4d13
Nolan <saturnsaturnsaturn17@gmail.com>
2025-12-05 11:43:52 +0100

    Automation_Start

--- ESP32_TEST/src/main.cpp
+++ ESP32_TEST/src/main.cpp
@@ -1,48 +1,46 @@
 
 #include <Arduino.h>
+
 #include "GPS.hpp"
 #include "RTC.hpp"
+#include "LDR.hpp"
+#include "BAT.hpp"
+#include "DHT.hpp"
 
-GPS gps(16, 17); // RX, TX
-RTC rtc;
+// Etat de l'automate
+enum State
+{
+  REPOS,
+  POWER_UP,
+  LUMINOSITY_MEASURE,
+  LUMINOSITY_IR,
+  PICTURE,
+  TEMP,
+  HUMIDITY,
+  HUMIDITY_TEMP,
+  GEOLOCATION,
+  DATE_TIME,
+  BATTERY,
+  POWER_DOWN
+};
+State state;
+
 void setup()
 {
   Serial.begin(115200);
-  while (!gps.gpsready())
-  {
-    Serial.println("En attente de données GPS...");
-    delay(1000);
-  }
-  gps.update();
-  rtc.updateFromGPS(gps.getHours(), gps.getMinutes(), gps.getSeconds(), gps.getDay(), gps.getMonth(), gps.getYear());
 
-  Serial.println("Données GPS reçues et RTC mise à jour.");
-
-  Serial.print("Latitude: ");
-  Serial.println(gps.getLatitude());
-  Serial.print("Longitude: ");
-  Serial.println(gps.getLongitude());
-  Serial.print("Heure UTC: ");
-  Serial.print(gps.getHours());
-  Serial.print(":");
-  Serial.print(gps.getMinutes());
-  Serial.print(":");
-  Serial.println(gps.getSeconds());
-  Serial.print("Date (JJ/MM/AAAA): ");
-  Serial.print(gps.getDay());
-  Serial.print("/");
-  Serial.print(gps.getMonth());
-  Serial.print("/");
-  Serial.println(gps.getYear());
-
-  Serial.println("===============================");
+  state = REPOS;
 }
+
 void loop()
 {
-  String dateTime = rtc.getDateTime();
-  Serial.println("Date et Heure RTC : " + dateTime);
-
-  delay(10000); // Attendre 10 secondes avant la prochaine mise à jour
-
-
+  switch (state)
+  {
+    case REPOS:
+      
+      break;
+    
+    default:
+      break;
+  }
 }
