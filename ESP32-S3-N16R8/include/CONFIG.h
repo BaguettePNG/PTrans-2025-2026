a7c8f1f57290ca9b3989a0b259df383fb5dec1a5
Nolan <saturnsaturnsaturn17@gmail.com>
2026-01-28 17:38:28 +0100

    Update_28_01_2025

--- ESP32-S3-N16R8/include/CONFIG.h
+++ ESP32-S3-N16R8/include/CONFIG.h
@@ -7,11 +7,12 @@
 // Macro pour débogage
 #define DEBUG_PRINT(msg) if(DEBUG_SD) { Serial.println(msg); }
 
+// Pin pour alimentation et réveil
 #define WAKE_UP           1
 #define PWR_U_D           2
 
 // Niveau de batterie
-#define BAT_PIN           19
+#define BAT_PIN           47
 
 // DHT22 sensor
 #define DHT_PIN           38
@@ -19,7 +20,12 @@
 // Brightness sensor
 #define LDR_PIN           -1 // Not Use
 
+// UART3 - Commincation
+#define WPBIS_RXD_2       41
+#define WPBIS_TXD_2       42
+
 // UART2 - GPS
+#define GPS_PWR_UD        21
 #define GPS_RX_PIN        18
 #define GPS_TX_PIN        17
 
@@ -32,8 +38,8 @@
 // CAMERA PINS
 #define SIOD_GPIO_NUM     8 // SDA
 #define SIOC_GPIO_NUM     9 // SCL
-#define D0_GPIO           41
-#define D1_GPIO           42
+#define D0_GPIO           46
+#define D1_GPIO           3
 #define D2_GPIO           16
 #define D3_GPIO           15
 #define D4_GPIO           7
@@ -42,7 +48,8 @@
 #define D7_GPIO           4
 #define VSYNC_GPIO        40
 #define HREF_GPIO         39
-#define PCLK_GPIO         3
+#define PCLK_GPIO         14
+#define RST_GPIO          -1 // Connect to GND
 #define XCLK_GPIO         -1 // Not Use  
 
 
